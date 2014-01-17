98a9d2e4eadd33a056b56e48a1bd21ad51103075
Diana Gamez <diana.gamez.fon@gmail.com>
2014-01-17 13:25:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playa::Application.config.secret_key_base = '4bbbe2f13cc320eedf46bae7f4201d4baabf870d50dcaf90f3f5d1ed9a312e04edebb2106285d22cfb2fd27bdc1765f3511bf6d62abc7bbb709ec5d98d89a549'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Playa::Application.config.session_store :cookie_store, key: '_Playa_session'
